b0bd96bae35567b35f343d5a4dea76097345f2a3
lalliere <58668197+lalliere@users.noreply.github.com>
2020-02-02 12:44:36 -0500

    coding for colorClasses added

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ let date = moment().format('dddd, MMMM Do');
 $("#currentDay").html(date);
 
 
+
+for (i=0; i <= 8; ++i) {
+    let time = parseInt($(".rows").attr("data-id"));
+    
+    if (time < moment().hour()) {
+        classColor = "past";
+        
+    } else if (time === moment().hour()) {
+        classColor = "present";
+        
+    } else {
+        classColor = "future";
+    }
+    
+    $(".rows").addClass(classColor);
+    console.log(time);
+    console.log(moment().hour());
+       
+}
